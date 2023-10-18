ff01a3e00c07c7d94d21b4ef35fd4bdc807b1f58
Jessica Wong <jessicaa.wong@mail.utoronto.ca>
2023-10-18 00:47:45 -0400

    Basic login page - will fix spacing later

--- betula.py
+++ betula.py
@@ -1,7 +1,30 @@
+from datetime import datetime
 from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+class NameForm(FlaskForm):
+    email = StringField('Username (your UofT Email address)', validators=[DataRequired(), Email()])
+    password = StringField('Password', validators=[DataRequired()])
+    login = SubmitField('Login')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    email = None
+    password = None
+    form = NameForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+    return render_template('index.html', form=form, email=email, password=password)
