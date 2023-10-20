0b45ef095141cbf55c0cf20cdb1583c4b850c5e6
Sean Pourgoutzidis <seanpourgoutzidis@Seans-MacBook-Air.local>
2023-10-19 20:38:09 -0400

    Developed feature to render a list of events. HTML is available in event.html and a demo can be seen by going to the /eventdemo page. Modified betula.py to include demo of event rendering. Additionally, fleshed out the READMe to include important project information.

--- betula.py
+++ betula.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
+from flask_moment import Moment
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/eventsdemo', methods=['GET', 'POST'])
+def events():
+    events = [['Event Name', 'Host Name' , "Social, Sports", "Description"], ['Event Name 2', 'Host Name 2' , "Free, Food", "Description 2"], ['Event Name 3', 'Host Name 3' , "Professional", "Description 3"]]
+    return render_template('event.html', events=events)
