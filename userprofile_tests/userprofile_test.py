6a8d966a75fdba300abae4f09aa1f6f6e5bd7550
lmtui <99363546+lmtui@users.noreply.github.com>
2023-10-29 14:24:50 -0400

    Update added name to userprofile_test.py

--- userprofile_tests/userprofile_test.py
+++ userprofile_tests/userprofile_test.py
@@ -9,7 +9,7 @@ def client():
     app.config['TESTING'] = True  
     with app.test_client() as client:
         yield client
-
+#lisa's Test case
 # Test the userprofile route for name update.
 def test_userprofile_update_name(client):
     response = client.post('/userprofile', data={"name": "NewName"})
