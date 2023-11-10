a9a4e5108726172bfd0d322748ad22e4a11308bc
Gordon Lin <75815453+hallovera@users.noreply.github.com>
2023-11-10 01:33:53 -0500

    Implement proper dashboard redirects to login; remove bugs

--- betula.py
+++ betula.py
@@ -150,7 +150,7 @@ def dashboard():
     try:
         get_user_table_data_query = f"SELECT * FROM USER_DATA WHERE User_Email = \'{session['email']}\'"
     except:
-        redirect('/login')
+        return redirect('/login')
 
     select_user_cursor = connection.cursor()
     select_user_cursor.execute(get_user_table_data_query)
